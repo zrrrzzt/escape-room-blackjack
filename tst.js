67d13b412ec2403efe404603217a508a135a8533
Geir Gåsodden <geir.gasodden@pythonia.no>
2018-12-22 09:30:22 +0100

    Updates player function. Fixes logic

--- tst.js
+++ tst.js
@@ -3,6 +3,8 @@
   const createPlayer = require('./lib/create-player')
   const isWinner = require('./lib/is-winner')
   let deck = createDeck()
+  let table = 0
+  let gotWinner = []
   deck.shuffle()
   const playerOne = createPlayer({
     deck: deck,
@@ -14,26 +16,28 @@
     name: 'Grusomme Gunnar',
     cash: 5
   })
-  playerOne.bet(1)
-  playerTwo.bet(1)
-  playerOne.draw()
-  playerOne.draw()
-  playerTwo.draw()
-  playerTwo.draw()
+  table += playerOne.bet(1)
+  table += playerTwo.bet(1)
+  console.log(table)
+  playerOne.draw({ cards: 2 })
+  playerTwo.draw({ cards: 2 })
   console.log(playerOne.showHand())
   console.log(playerTwo.showHand())
-  console.log(isWinner(playerOne, playerTwo))
+  gotWinner = isWinner(playerOne, playerTwo)
+  if (gotWinner.length === 1) {
+    const winner = gotWinner[0]
+    console.log(winner.showStats())
+    table -= winner.win(table)
+    gotWinner = []
+  }
   deck.newGame()
   deck.shuffle()
   playerOne.newGame()
   playerTwo.newGame()
   playerOne.bet(1)
   playerTwo.bet(1)
-  playerOne.draw()
-  playerOne.draw()
-  playerTwo.draw()
-  playerTwo.draw()
+  playerOne.draw({ cards: 2 })
+  playerTwo.draw({ cards: 2 })
   console.log(playerOne.showHand())
   console.log(playerTwo.showHand())
-  console.log(isWinner(playerOne, playerTwo))
 })()
